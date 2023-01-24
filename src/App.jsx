0ca71a9a0adc7d36968f7947c650163628fdb253
Brian <bstanton773@gmail.com>
2023-01-24 11:45:57 -0600

    Add ToDo component with ToDoForm and ToDoCards for each task

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav';
+import ToDo from './components/ToDo';
 
 export default function App() {
     return (
         <>
             <Nav />
             <div className="container">
-                <h1 className='text-center'>Hello World</h1>
+                <ToDo />
             </div>
         </>
     )
