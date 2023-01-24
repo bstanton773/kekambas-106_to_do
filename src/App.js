f84bf24ab8d2e9c27e2e088962bbc8ecaa2adda2
Brian <bstanton773@gmail.com>
2023-01-24 10:45:01 -0600

    Add Nav

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function App() {
-    return (
-        <h1 className='text-center'>Hello World</h1>
-    )
-}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+export default function App() {
+    return (
+        <>
+            <Nav />
+            <div className="container">
+                <h1 className='text-center'>Hello World</h1>
+            </div>
+        </>
+    )
+}
